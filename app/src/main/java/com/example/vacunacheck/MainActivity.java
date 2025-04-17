b18b0d0c15a6d1565bf5e4e1ba4c05d8ecb7329d
MiguelEspinoza04 <yoangel1221@gmail.com>
2025-04-17 15:16:18 -0500

    creando proyecto

---
+++ app/src/main/java/com/example/vacunacheck/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.vacunacheck;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextUsername, editTextPassword;
+    Button buttonLogin;
+
+    private final String USERNAME = "admin";
+    private final String PASSWORD = "1234";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(view -> {
+            String username = editTextUsername.getText().toString();
+            String password = editTextPassword.getText().toString();
+
+            if (username.equals(USERNAME) && password.equals(PASSWORD)) {
+                Toast.makeText(MainActivity.this, "Login exitoso", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, MainPage.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
+    }
+}
